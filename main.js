73f57a5e5513d7237b2d284546122a539efb237d
Tom Herrmann <tomherrmanndev@gmail.com>
2019-09-18 19:26:48 -0400

    added div variables in js

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 //TIME
 //Date.now - look it up
+const time = document.querySelector('#time');
 
 //DATE
 //see above
+const date = document.querySelector('#date');
 
 //WEATHER
 //get from api
 //code for local area
+const weather = document.querySelector('#weather');
 
 //NEWS
 // get api
 // create scroll across the footer area
+const news = document.querySelector('#news');
+
+//BACKGROUND
+// look into this
