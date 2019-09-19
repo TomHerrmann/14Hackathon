7f5a0949a593382ba07aec951d4f86d2cccd54cb
RajeebTheGreat <rajeebthegreat@gmail.com>
2019-09-19 10:07:24 -0400

    Fix weather location

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const weatherAPIURL =
-  'http://api.openweathermap.org/data/2.5/weather?id=5128638&units=imperial&APPID=fd855b304a1527194bbd531dbe4f61e2';
+  'http://api.openweathermap.org/data/2.5/weather?zip=10013&units=imperial&APPID=fd855b304a1527194bbd531dbe4f61e2';
 
 const weatherData = fetch(weatherAPIURL)
   .then(function(response) {
@@ -7,7 +7,7 @@ const weatherData = fetch(weatherAPIURL)
   })
   .then(function(response) {
     const iconID = response['weather'][0]['icon'];
-    document.getElementById('temp').innerHTML = `${Math.floor(response['main']['temp'])} F`;
+    document.getElementById('temp').innerHTML = `${Math.ceil(response['main']['temp'])} F`;
 
     // Image
     const weatherIcon = document.createElement('img');
