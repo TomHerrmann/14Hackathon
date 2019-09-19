939cb92342910d61e4bf250173c17c237dee5187
Tom Herrmann <tomherrmanndev@gmail.com>
2019-09-19 11:57:52 -0400

    arrayed

--- main.js
+++ main.js
@@ -118,11 +118,19 @@ const makeNewsSection = articlesArr => {
 
 // news API key
 
+const breakingNews = [];
+
 const newsAPI =
   'https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=27670d367cd8498ea7d02d5201f9de1c';
 
-const breakingNews = fetch(newsAPI)
+fetch(newsAPI)
   .then(function(response) {
     return response.json();
   })
-  .then(elem => makeNewsSection(elem.articles));
+  .then(data =>
+    data.articles.forEach(elem => {
+      breakingNews.push(elem);
+    })
+  );
+
+console.log('newsArr', breakingNews);
